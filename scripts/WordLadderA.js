9864fd159053b057aaa42c7485e2ccd6fa257e60
Charlie Lipford <charlie.lipford.code@gmail.com>
2012-08-29 16:51:16 -0400

    added functionality to choose the start and end of the latter

--- scripts/WordLadderA.js
+++ scripts/WordLadderA.js
@@ -1,13 +1,36 @@
 var socket = io.connect('http://127.0.0.1:8000');
 window.onload = function(){
 
-//Setup Buttons
+//Setup Buttons================================================================
 document.getElementById('Climb').onclick = climb;
 
-//functions
+//wont need this when the list is dynamically added
+var wordlist = document.getElementById('wordlist'),
+    words    = wordlist.getElementsByClassName('styledListElement'),
+    from     = document.getElementById('fromlabel'),
+    to       = document.getElementById('tolabel'),
+    fromto   = 0;
+
+	for(w in words){ 
+		words[w].onclick = setLadderPoles;
+	};
+
+//functions====================================================================
 
 function climb(){
 	alert('Climbing Word Ladder!');
 }
+
+function setLadderPoles(e){ 
+	if(fromto == 0){
+		from.innerText = 'From: ' + e.target.innerText;
+		fromto++;
+	}
+	else{
+		to.innerText = 'To: ' + e.target.innerText;
+		fromto = 0;
+	}
+}
+
 
 }
