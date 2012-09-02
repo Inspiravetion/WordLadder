a434438e0210943f71f91e578743fec51fa28461
Charlie Lipford <charlie.lipford.code@gmail.com>
2012-09-02 03:56:33 -0400

    commented/cleaned up code

--- serverside/WordLadderServer.js
+++ serverside/WordLadderServer.js
@@ -73,15 +73,21 @@ function bootstrapRCSS() {
 
 //SOCKET SETUP=================================================================
 io.sockets.on('connection', function(socket){
-	var A = new Alphabet();
-	console.log(A);
 
-	
 
 });
 
 //CLIMBING ALGORITHM===========================================================
 
+/*
+ * Takes a start/end string along with a dictionary object and starts the word 		
+ * ladder climbing process by first getting a reference to the start/end Word 		
+ * objects in the dictionary and then calling solve() with the last param as 		
+ * 'true' to denote that it is the master caller.		
+ * @param start is the string that should be at the top of the word ladder 		
+ * @param end is the string that should be at the bottom of the word ladder 		
+ * @param dict is the dictionary object that holds all the Word objects 
+ */ 
 climb = function(start, end, dict){
 	var startTime = new Date().getMilliseconds();
 	var top, bottom;
@@ -96,7 +102,7 @@ climb = function(start, end, dict){
 			break;
 		}
 	}
-	//reporting the results
+	//reporting the results...this will change
 	var answer = solve(top, top, bottom, [], true);
 	answer.sort(function(a,b){
 		if((a.split(' ').length) > (b.split(' ').length)){
@@ -114,6 +120,22 @@ climb = function(start, end, dict){
 	console.log('\n\nTime Check: ' + (endTime - startTime) + ' milliseconds');
 };
 
+/*
+ * This is the recursive part of the solution. It takes in the current word 		
+ * being assessed, the word to start with, the word to end with, an array 		
+ * of words that have already been checked in the current path, and a boolean 		
+ * flag denoting whether the caller is the top of the recursive stack. Then 		
+ * checks if the current word is the target word, if the recursion should pop 		
+ * back(hits the start word, current word has no similar words, or current 		
+ * word has already been checked). If not, the method is called recursively 		
+ * until all the possible paths have been explored/reported. 		
+ * @param current is the current Word object being assessed 		
+ * @param start is the Word object that is at the top of the word ladder 		
+ * @param target is the Word object that is at the end of the word ladder		
+ * @param checked is an array of all the checked words in the word ladder path 		
+ * @param startFlag is a boolean that denotes whether the caller is the first 		
+ * one in the recursive call
+ */ 	
 solve = function(current, start, target, checked, startFlag){
 	if(current === target){
 		return [current.value];
@@ -143,128 +165,6 @@ solve = function(current, start, target, checked, startFlag){
 
 //CLASSES======================================================================
 
-Letter = function(character){
-	var self       = this;
-	this.parents   = [];
-	this.children  = [];
-	this.character = character;
-	this.addParent = function(p){
-		if(typeof p == Letter){
-			self.parents.push(p);
-		}
-	};
-	this.addChild  = function(c){
-		if(typeof c == Letter){
-			self.children.push(c);
-			c.addParent(this);
-		}
-	};
-	this.character = function(){
-		return self.character;
-	};
-	return this;
-};
-
-Alphabet = function(){
-	var self   = this,
-	characters = ['a','b','c','d','e','f','g','e','f','h','i','j','k','l',
-	'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-	for(c in characters){
-		self[characters[c]] = new Letter(characters[c]);
-	}
-	return this;
-};
-
-NestedBST = function(rootData){
-	var self = this;
-	this.root  = new BST(rootData);
-
-	this.add = function(data){
-		var child = new BST(data);
-		self.root.addChild(child);
-	};
-
-	this.print = function(){
-		var spaces = 0;
-		console.log('\n' + 'Nested Tree:\n');
-		self.root.print(0);
-	}; 
-};
-
-BST = function(data){
-	var self         = this;
-	this.payLoad     = data;
-	this.leftChild   = null;
-	this.rightChild  = null;
-	this.nestedRight = null;
-	this.nestedLeft  = null;
-	
-	this.addChild   = function(child, nest){
-		var left, right;
-		if(nest){
-			left  = 'nestedLeft';
-			right = 'nestedRight';
-		}
-		else{
-			left  = 'leftChild';
-			right = 'rightChild'; 
-		}
-		if(child instanceof BST && child.payLoad){
-			if(child.payLoad < self.payLoad){
-				if(self[left]){
-					self[left].addChild(child, nest);
-				}
-				else{
-					self[left] = child;
-				}
-				return true;
-			} 
-			else{
-				if(self[right]){
-					self[right].addChild(child, nest);
-				}
-				else{
-					self[right] = child;
-				}
-				return true;
-			}
-		}
-		else if (!child || !child instanceof BST){
-			console.log('ERROR:: Child must be an instance of BST.');
-			return false;
-		}
-		else {
-			console.log('ERROR:: Child has no data in its payload.');
-			return false;
-		}
-	};
-
-	this.addNestedChild = function(child){
-		self.addChild(child, true);
-	};
-
-	this.print = function(spaces){
-		var padding = '';
-		for(var i = 0; i < spaces; i++){
-			padding += '  ';
-		}
-		console.log(padding + self.payLoad + '\n');
-		if(self.nestedLeft){
-			self.nestedLeft.print(spaces + 1);
-		}
-		if(self.nestedRight){
-			self.nestedRight.print(spaces + 1);
-		}
-		if(self.leftChild){
-			self.leftChild.print(spaces + 1);
-		}
-		if(self.rightChild){
-			self.rightChild.print(spaces + 1);
-		}
-	}
-
-};
-
 Dictionary = function(path){
 	var file = fs.readFileSync(__dirname + path,'utf8'),
 	//TODO FIGURE OUT HOW TO DETERMINE LIKE TERNIMNATION CHARACTER
@@ -273,14 +173,18 @@ Dictionary = function(path){
 	self     = this,
 	wordObjs = [];
 	for(w in words){
-		//store just words to be displayed to the client
 		wordObjs.push(new Word(words[w]));
 	}
 	linkWords(wordObjs);
-	//console.log(wordObjs); 
 	return wordObjs;
 };
 
+/*
+ * Creates a Word object from a string. The word object has methods and 		
+ * functions to help calculate the word ladders.		
+ * @param word is the string word that the Word object is to represent
+ * @return a Word object
+ */ 	
 Word = function(word){
 	var self     = this;
 	this.length  = word.length;
@@ -313,16 +217,6 @@ Word = function(word){
 
 //Testing
 
-	var nbst = new NestedBST('c');
-	nbst.add('a');
-	nbst.add('u');
-	//nbst.print();
-	nbst.root.leftChild.addNestedChild(new BST('t'));
-	nbst.root.leftChild.addNestedChild(new BST('p'));
-	nbst.root.rightChild.addNestedChild(new BST('t'));
-	nbst.root.rightChild.addNestedChild(new BST('p'));
-	//nbst.print();
-
 	var dict = new Dictionary('/../serverside/dictionary.txt');
 	console.log('STONE -> MONEY:');
 	climb('stone', 'money', dict);
@@ -333,7 +227,11 @@ Word = function(word){
 //HELPERS======================================================================
 
 
-
+/*
+ * Bi-directionally links two Word objects that are one letter away from 
+ * eachother so that they have references to eachother for the climbing process		
+ * @param array is an Array of Word objects (Dictionary)
+ */ 
 function linkWords(array){
 	for(var i = 0; i < array.length - 1; i++){
 		for(var j = i + 1; j < array.length; j++){
@@ -344,6 +242,11 @@ function linkWords(array){
 	}
 };
 
+/*
+ * Checks to see if the two words are 1 character away from eachother or not 		
+ * @param stringA is the first string to be compared 		
+ * @param stringB is the second string to be compared
+ */ 
 function oneAway(stringA, stringB){
 	var diff = 0;
 	for(var i = 0; i < stringA.length; i++){
@@ -354,6 +257,11 @@ function oneAway(stringA, stringB){
 	return diff == 1? true: false; 
 };
 
+/*
+ * Removes the given value from the given array		
+ * @param val is the value to be removed		
+ * @param array is the array that val should be removed from
+ */ 
 function remove(val, array) {
 	if(array.indexOf(val) != -1){
 		array.splice(array.indexOf(val), 1);
